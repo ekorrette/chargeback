98ac7c93e33aa0d7f36ec73487aa1420376d3956
KubinGH <kbachurski@gmail.com>
2020-12-28 18:23:18 +0100

    Ziuum

--- www/index.js
+++ www/index.js
@@ -1,9 +1,31 @@
-import { memory } from "chargeback/chargeback_bg";
+// import { memory } from "chargeback/chargeback_bg";
 
 import * as wasm from "chargeback-game";
 
 wasm.greet();
 
 const canvas = document.getElementById("chargeback-canvas");
-canvas.height = 600;
-canvas.width = 800;
+canvas.width = 600;
+canvas.height = 800;
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.rect(20, 20, 150, 100);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.rect(40, 40, 150, 100);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
