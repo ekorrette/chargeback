1d1d1849d652164c9252379e9238c528e1de2cc9
KubinGH <kbachurski@gmail.com>
2020-12-28 18:16:01 +0100

    Zium

--- www/index.js
+++ www/index.js
@@ -1,3 +1,9 @@
+import { memory } from "chargeback/chargeback_bg";
+
 import * as wasm from "chargeback-game";
 
 wasm.greet();
+
+const canvas = document.getElementById("chargeback-canvas");
+canvas.height = 600;
+canvas.width = 800;
